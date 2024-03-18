d2f54dd52b4047bdfd7ba0ba8696e1ac7d752266
forest <alexeidorman@gmail.com>
2024-03-18 11:16:24 +0900

    update_3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
-
+app.use(cors())
 app.get('/', (req, res) => {
   res.json({url : 'https://expressjs.com/en/starter/hello-world.html'})
 })
