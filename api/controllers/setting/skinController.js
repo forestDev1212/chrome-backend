26943203d711d9909bb96dd811b222198a965166
forest <alexeidorman@gmail.com>
2024-04-02 01:16:11 +0900

    skin controller update

--- api/controllers/setting/skinController.js
+++ api/controllers/setting/skinController.js
@@ -1,137 +1,164 @@
-import {Skin} from "../../../models/index.js"
+import { Skin } from "../../../models/index.js";
 
 const create = async (req, res) => {
   try {
-    const data = req.body
+    const data = req.body;
     const newDoc = new Skin({
-      ...data
-    })
+      ...data,
+    });
     await newDoc.save();
     return res.json({
-      success : true,
-      message : "Successfully Created!"
-    })
+      success: true,
+      message: "Successfully Created!",
+    });
   } catch (err) {
-    console.log("Error : ", err.message)
+    console.log("Error : ", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
 
 const list = async (req, res) => {
   try {
     const result = await Skin.find({
-      delFlag : false
-    })
+      delFlag: false,
+    });
     res.status(200).json({
-      success : true,
-      data : result
-    })
+      success: true,
+      data: result,
+    });
   } catch (err) {
     console.log("Error :", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
 
 const listOne = async (req, res) => {
   try {
-    const {id} = req.query
+    const { id } = req.query;
     const result = await Skin.findOne({
-      delFlag : false,
-      _id : id
-    })
+      delFlag: false,
+      _id: id,
+    });
     res.status(200).json({
-      success : true,
-      data : result
-    })
+      success: true,
+      data: result,
+    });
   } catch (err) {
     console.log("Error :", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
 
 const update = async (req, res) => {
   try {
     const data = req.body;
-    const {id} = data
+    const { id } = data;
     const updateResult = await Skin.findByIdAndUpdate(id, data);
-    if(!updateResult) {
+    if (!updateResult) {
       return res.status(404).json({
-        success : false,
-        message : "No Result with id: " + id
-      })
+        success: false,
+        message: "No Result with id: " + id,
+      });
     } else {
       res.status(200).json({
-        success : true,
-        message : "Successfully Updated!"
-      })
+        success: true,
+        message: "Successfully Updated!",
+      });
     }
   } catch (err) {
     console.log("Error :", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
 
 const remove = async (req, res) => {
   try {
-    const {id} = req.query
+    const { id } = req.query;
     const removedResult = await Skin.updateOne(id, {
-      delFlag : true
-    })
-    if(!removedResult) {
+      delFlag: true,
+    });
+    if (!removedResult) {
       return res.json({
-        success : false,
-        message : "No Result with id: " + id
-      })
+        success: false,
+        message: "No Result with id: " + id,
+      });
     } else {
       res.status(200).json({
-        success : true,
-        message : "Successfully removed."
-      })
+        success: true,
+        message: "Successfully removed.",
+      });
     }
   } catch (err) {
-    console.log("Error : ", err.message)
+    console.log("Error : ", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
 
 const deleteOne = async (req, res) => {
   try {
-    const {id} = req.query
-    const deletedResult = await Skin.deleteOne(id)
-    if(!deletedResult) {
+    const { id } = req.query;
+    const deletedResult = await Skin.deleteOne(id);
+    if (!deletedResult) {
       return res.json({
-        success : false,
-        message : "No Result with id: " + id
-      })
+        success: false,
+        message: "No Result with id: " + id,
+      });
     } else {
       res.status(200).json({
-        success : true,
-        message : "Successfully deleted."
-      })
+        success: true,
+        message: "Successfully deleted.",
+      });
     }
   } catch (err) {
-    console.log("Error : ", err.message)
+    console.log("Error : ", err.message);
     res.json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
-}
+};
+
+const applyNewSkin = async () => {
+  try {
+    const { id } = req.query;
+    // Update documents where applied is true
+    const removeResults = await Skin.updateMany(
+      { applied: true },
+      { applied: false }
+    );
+    if (!removeResults) {
+      console.log(` documents updated`);
+      const updateResult = await Skin.findByIdAndUpdate(id, { applied: true });
+      if (!updateResult) {
+        res.status(200).json({
+          success: true,
+          message: "Successfully deleted.",
+        });
+      }
+    }
+  } catch (err) {
+    console.log("Error : ", err.message);
+    res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
 export default {
   create,
@@ -139,5 +166,5 @@ export default {
   list,
   listOne,
   deleteOne,
-  remove
-}
+  remove,
+};
